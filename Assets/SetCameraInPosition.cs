4df9b6ae6aa705acae6e90b285108b643bcc8526
GrayBreath <sinchik97@mail.ru>
2020-11-20 11:08:14 +0200

    TavernInterview

---
+++ Assets/SetCameraInPosition.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCameraInPosition : MonoBehaviour
+{
+
+    public GameObject pos1, pos2, pos3;
+    public Vector3 from_pos, to_pos;
+    public Quaternion from_rot, to_rot;
+
+
+    public float Lerper;
+    public bool StartLerp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ToPos1() 
+    {
+        Lerper = 0;
+        from_pos = transform.position;
+        to_pos = pos1.transform.position;
+        from_rot = transform.rotation;
+        to_rot = pos1.transform.rotation;
+        StartLerp = true;
+    }
+
+    public void ToPos2()
+    {
+        Lerper = 0;
+        from_pos = transform.position;
+        to_pos = pos2.transform.position;
+        from_rot = transform.rotation;
+        to_rot = pos2.transform.rotation;
+        StartLerp = true;
+    }
+    public void ToPos3()
+    {
+        Lerper = 0;
+        from_pos = transform.position;
+        to_pos = pos3.transform.position;
+        from_rot = transform.rotation;
+        to_rot = pos3.transform.rotation;
+        StartLerp = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (StartLerp == true) 
+        {
+            Lerper += Time.deltaTime;
+            transform.position = Vector3.Lerp(from_pos,to_pos,Lerper);
+            transform.rotation= Quaternion.Lerp(from_rot, to_rot, Lerper);
+            if (Lerper > 1) StartLerp = false;
+        }
+
+    }
+}
---
+++ Assets/VRTriggerButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VRTriggerButton : MonoBehaviour
+{
+    
+    public UnityEvent Command;
+    // Start is called before the first frame update
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Command.Invoke();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
