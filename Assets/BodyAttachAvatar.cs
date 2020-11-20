8a1c2e152140e32840c484f404df8a7615bc093c
GrayBreath <sinchik97@mail.ru>
2020-11-20 16:27:07 +0200

    InterviewInit

---
+++ Assets/BodyAttachAvatar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyAttachAvatar : MonoBehaviour
+{
+    public GameObject body_model;
+    public Vector3 body_y_offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+      body_model.transform.position =   transform.position+ body_y_offset ;
+
+      var b_rot = transform.eulerAngles;
+      b_rot.x=0;
+      b_rot.z=0;
+      body_model.transform.eulerAngles = b_rot;
+
+
+    }
+}
